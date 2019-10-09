6f1e27245be0cca89be09c10e5cc072094d4edef
Ben Keller <benk@nvidia.com>
2019-10-09 11:09:11 -0700

    Remove HLS defines that were associated with an old version of WHVCRouterTop.
    This closes #1.

--- hls/unittests/WHVCRouterTop/go_hls.tcl
+++ hls/unittests/WHVCRouterTop/go_hls.tcl
@@ -16,46 +16,9 @@ source ../../nvhls_exec.tcl
 
 set MAIN_PIPELINE_II 1
 
-set PACKETIDWIDTH 1
-
-
-set VC_LIST {1}
-set BUFF_LIST {4 }
-set NUM_LPORTS_LIST {1 }
-set NUM_RPORTS_LIST {2 }
-set FLIT_DATAWIDTH_LIST {64}
-
-foreach VC $VC_LIST {
-    foreach BUFFSIZE $BUFF_LIST {
-        foreach NUM_LPORTS $NUM_LPORTS_LIST {
-            foreach NUM_RPORTS $NUM_RPORTS_LIST {
-                foreach FLIT_DATAWIDTH $FLIT_DATAWIDTH_LIST {
-
-                    proc nvhls::set_compiler_flags {HLS_CATAPULT COMPILER_FLAGS} {
-                        global VC BUFFSIZE NUM_LPORTS NUM_RPORTS FLIT_DATAWIDTH
-                        if { $HLS_CATAPULT eq "1" } {
-                            set HLS_CATAPULT_FLAG "-DHLS_CATAPULT"
-                        } else {
-                            set HLS_CATAPULT_FLAG ""
-                        }
-                        set FLAG_STR ""
-                        foreach flag $COMPILER_FLAGS {
-                            append FLAG_STR "-D$flag "
-                        }
-                        options set Input/CompilerFlags "-D_SYNTHESIS_ $HLS_CATAPULT_FLAG $FLAG_STR -DNUM_LPORTS=$NUM_LPORTS -DNUM_RPORTS=$NUM_RPORTS -DFLIT_DATAWIDTH=$FLIT_DATAWIDTH -DNUM_VCHANNELS=$VC -DBUFFERSIZE=$BUFFSIZE"
-                    }
-
-                    proc nvhls::usercmd_post_assembly {} {
-                        global NUM_LPORTS NUM_RPORTS VC BUFFERSIZE
-                        upvar TOP_NAME TOP_NAME
-                        set DESIGN_TOP "/$TOP_NAME/core"
-
-                        directive set /WHVCRouterTop/WHVCSourceRouter<$NUM_LPORTS,$NUM_RPORTS,$VC,$BUFFSIZE,WHVCRouterTop::Flit_t,16>/process/while -PIPELINE_INIT_INTERVAL 1
-                    }
-
-                    nvhls::run
-                }
-            }
-        }
-    }
+proc nvhls::usercmd_post_assembly {} {
+    upvar TOP_NAME TOP_NAME
+    directive set /$TOP_NAME/WHVCSourceRouter<1,4,1,8,$TOP_NAME::Flit_t,16>/process/while -PIPELINE_INIT_INTERVAL 1
 }
+
+nvhls::run
