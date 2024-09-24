83c65f02059345bad0d7937944296c1a7394d55a
Rangharajan Venkatesan <rangharajanv@nvidia.com>
2024-09-24 11:39:39 -0700

    memory array changes for block size and assertions

--- cmod/include/mem_array.h
+++ cmod/include/mem_array.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2019,2024 NVIDIA CORPORATION.  All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
@@ -159,8 +159,129 @@ class mem_array_sep {
         m & bank[i][j];
       }
     }
-  } 
+  }
 };
 
+/**
+ * \brief Abstract Memory Class
+ * \ingroup MemArray
+ *
+ * \tparam T                       Datatype of an entry to be stored in memory
+ * \tparam NumEntriesPerBank       Number of entries per bank in memory
+ * \tparam NumBanks                Number of banks in memory
+ *
+ *
+ * \par A Simple Example
+ * \code
+ *      #include <mem_array.h>
+ *        ...
+ *        mem_array_opt <MemWord_t, NUM_ENTRIES_PER_BANK, NBANKS> banks;
+ *
+ *        read_data = banks.read(bank_addr, bank_sel);
+ *
+ *        banks.write(bank_addr, bank_sel, write_data);
+ *        ...
+ *
+ *
+ * \endcode
+ * \par
+ *
+ */
+
+
+template <typename T, int NumEntriesPerBank, int NumBanks=1, int NumByteEnables=1>
+class mem_array_opt {
+ public:
+  typedef Wrapped<T> WData_t;
+  static const unsigned int NumEntries = NumEntriesPerBank*NumBanks;
+  static const unsigned int WordWidth = WData_t::width;
+  static const unsigned int SliceWidth = WordWidth/NumByteEnables;
+  typedef NVUINTW(nvhls::index_width<NumEntriesPerBank>::val) LocalIndex;
+  typedef NVUINTW(nvhls::index_width<NumEntriesPerBank*NumByteEnables>::val) LocalSliceIndex;
+  typedef NVUINTW(nvhls::index_width<NumBanks>::val) BankIndex;
+  typedef sc_lv<WordWidth> Data_t;
+  typedef sc_lv<SliceWidth> Slice_t;
+  typedef NVUINTW(NumByteEnables) WriteMask;
+  typedef NVUINTW(nvhls::index_width<NumByteEnables>::val) ByteEnableIndex;
+
+  typedef Slice_t BankType[NumEntriesPerBank*NumByteEnables];
+  #pragma hls_block_size NumEntriesPerBank
+  BankType bank[NumBanks];
+  static const int width =  NumEntries * WordWidth;
+
+  mem_array_opt() {
+    Slice_t value;
+    for (unsigned i = 0; i < NumBanks; i++) {
+      for (unsigned j = 0; j < NumByteEnables* NumEntriesPerBank; j++) {
+#ifndef SLEC_CPC
+        bank[i][j] = value;
+#endif
+      }
+    }
+//     T value;
+//     for (unsigned i = 0; i < N; i++) {
+// #ifndef SLEC_CPC
+//       data[i] = value;
+// #endif
+//     }
+  }
+
+  void clear() {
+    Slice_t value = 0;
+    for (unsigned i = 0; i < NumBanks; i++) {
+      for (unsigned j = 0; j < NumByteEnables * NumEntriesPerBank; j++) {
+        bank[i][j] = value;
+      }
+    }
+  }
+
+  T read(LocalIndex idx, BankIndex bank_sel=0) {
+    Data_t read_data = TypeToBits<NVUINTW(WordWidth)>(0);
+    #pragma hls_unroll yes
+    for (int i = 0; i < NumByteEnables; i++) {
+      LocalSliceIndex local_slice_index = idx * NumByteEnables + i;
+      NVHLS_ASSERT_MSG(bank_sel<NumBanks, "bank index out of bounds");
+      NVHLS_ASSERT_MSG(idx<NumEntriesPerBank, "local index out of bounds");
+      read_data.range((i+1)*SliceWidth-1, i*SliceWidth) = bank[bank_sel][local_slice_index];
+    }
+    //NVHLS_ASSERT_NO_X(read_data);
+    //NVHLS_ASSERT_NO_X_MSG(read_data, "Read_data_is_X");
+    CMOD_ASSERT_MSG(read_data.xor_reduce()!=sc_logic('X'), "Read data is X");
+    return BitsToType<T>(read_data);
+  }
+
+  void write(LocalIndex idx, BankIndex bank_sel, T val, WriteMask write_mask=~static_cast<WriteMask>(0), bool wce=1) {
+    Slice_t tmp[NumByteEnables];
+    Data_t write_data = TypeToBits<T>(val);
+    #pragma hls_unroll yes
+    for (int i = 0; i < NumByteEnables; i++) {
+      tmp[i] = write_data.range((i+1)*SliceWidth-1, i*SliceWidth);
+    }
+    if (wce) {
+      #pragma hls_unroll yes
+      for (int i = 0; i < NumByteEnables; i++) {
+        if (write_mask[i] == 1) {
+          LocalSliceIndex local_slice_index = idx * NumByteEnables + i;
+          NVHLS_ASSERT_MSG(bank_sel<NumBanks, "bank index out of bounds");
+          NVHLS_ASSERT_MSG(idx<NumEntriesPerBank, "local index out of bounds");
+          bank[bank_sel][local_slice_index] = tmp[i];
+          //NVHLS_ASSERT_NO_X(tmp[i]);
+          //NVHLS_ASSERT_NO_X_MSG(tmp[i], "Write_data_is_X" );
+          CMOD_ASSERT_MSG(tmp[i].xor_reduce()!=sc_logic('X'), "Write data is X");
+        }
+      }
+    }
+  }
+
+  template<unsigned int Size>
+  void Marshall(Marshaller<Size>& m) {
+    for (unsigned i = 0; i < NumBanks; i++) {
+      for (unsigned j = 0; j < NumByteEnables* NumEntriesPerBank; j++) {
+        m & bank[i][j];
+      }
+    }
+  }
+
+};
 
 #endif
---
+++ cmod/unittests/MemArrayOpt/testbench.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016-2019, NVIDIA CORPORATION.  All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdio.h>
+#include <match_scverify.h>
+#include <testbench/nvhls_rand.h>
+
+#include "MemArrayOpt.h"
+#include "mem_config.h"
+
+#ifndef NUM_ITER
+#define NUM_ITER 1000
+#endif
+
+MemWord_t wideRand ();
+
+CCS_MAIN(int argc, char *argv[]) {
+
+  nvhls::set_random_seed();
+
+  static const int kDebugLevel = 1;
+
+  const int count = NUM_ITER;
+  int start = 0;
+
+
+  MemAddr_t address[count];
+  MemWord_t ref[NUM_ENTRIES];
+  MemWord_t write_data;
+  MemWord_t read_data;
+
+  /* Do a write and a read of zero first to clear out some x's */
+  MemOp_t mem_op = WRITE;
+  MemAddr_t addr = 0;
+  write_data = 0;
+  ref[addr] = write_data;
+  CCS_DESIGN(MemArrayOpt)(write_data, read_data, mem_op, addr);
+  mem_op = READ;
+  CCS_DESIGN(MemArrayOpt)(write_data, read_data, mem_op, addr);
+
+
+  for (int i = start; i< start+count; i++) {
+    MemOp_t mem_op = WRITE;
+    MemAddr_t addr = rand() % NUM_ENTRIES;
+    write_data = wideRand();
+    //MemAddr_t addr = i;
+    address[i] = addr;
+    ref[addr] = write_data;
+    CDCOUT("Memory Op: Write, Address: " << hex << addr.to_uint64() << " Data: " << write_data.to_uint64() << endl, kDebugLevel);
+    CCS_DESIGN(MemArrayOpt)(write_data, read_data, mem_op, addr);
+  }
+
+  for (int i = start; i< start+count; i++) {
+    MemOp_t mem_op = READ;
+    MemAddr_t addr = address[i];
+    CCS_DESIGN(MemArrayOpt)(write_data, read_data, mem_op, addr);
+    CDCOUT("Memory Op: Read, Address: "<< addr.to_uint64() << " Data: "<< read_data.to_uint64() << " " << ref[addr] << endl, kDebugLevel);
+    assert(ref[addr]==read_data);
+  }
+  CCS_RETURN(0) ;
+
+}
+
+MemWord_t wideRand () {
+  MemWord_t retval;
+  if (WORDSIZE<=32) {
+    retval = rand();
+  } else {
+    NVUINTC(32) tmp;
+    for (int bitpos=0; bitpos<WORDSIZE; bitpos+=32) {
+      tmp = rand();
+      retval = nvhls::set_slc(retval, tmp, bitpos);
+    }
+  }
+  return retval;
+}
